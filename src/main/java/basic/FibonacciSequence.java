5a151f3f02ac95d89a1ec3740c0f6bc99ef3a736
pencejz <pencejz@163.com>
2018-07-28 14:28:05 +0800

    项目初始化

---
+++ src/main/java/basic/FibonacciSequence.java
@@ -0,0 +1,15 @@
+package basic;
+
+/**
+ * 斐波拉契数列
+ * @author pencejz
+ * 2018年7月28日 下午2:22:58
+ */
+public class FibonacciSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
